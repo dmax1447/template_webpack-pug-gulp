0b359af1a24f3a8d262e61eda2b0ba6c1a1bc0fa
morglod <morglod@gmail.com>
2019-04-24 07:03:21 +0300

    small refactor

---
+++ src/core/utils-mobile.ts
@@ -0,0 +1,151 @@
+const DetectMobileBrowser = require("detect-mobile-browser")(false);
+
+export function detectTouch(
+    touchDetected: () => void,
+): () => void {
+    const ontouch = () => {
+        destroy();
+        touchDetected();
+    };
+
+    const onpointer = (evt: PointerEvent) => {
+        if (evt.pointerType !== 'mouse') {
+            ontouch();
+        }
+    };
+
+    window.addEventListener('touchstart', ontouch);
+    window.addEventListener('touchmove', ontouch);
+    window.addEventListener('pointerdown', onpointer);
+    window.addEventListener('pointermove', onpointer);
+
+    const destroy = () => {
+        window.removeEventListener('touchstart', ontouch);
+        window.removeEventListener('touchmove', ontouch);
+        window.removeEventListener('pointerdown', onpointer);
+        window.removeEventListener('pointermove', onpointer);
+    };
+
+    return destroy;
+}
+
+
+let __isMobileHookScreen: boolean|undefined = undefined;
+
+export function isMobileScreen() {
+    return (__isMobileHookScreen !== undefined && __isMobileHookScreen) || DetectMobileBrowser.isAny() || window.innerWidth <= 571;
+}
+
+export function __unsafe_setIsMobileScreen(hook: boolean) {
+    __isMobileHookScreen = hook;
+}
+
+// ----------------------------
+//           SWIPE
+// ----------------------------
+
+export function startDetectingScreenSwipe(
+    onSwipe: (direction: 'up'|'down'|'left'|'right') => void,
+    onCancel?: () => void,
+    deadZone = 15,
+) {
+    let initialPos: undefined|{ x: number, y: number };
+
+    const mouseMove = (ev: MouseEvent) => {
+        initialPos = {
+            x: ev.clientX,
+            y: ev.clientY,
+        };
+        window.removeEventListener('mousemove', mouseMove);
+        window.removeEventListener('touchmove', touchMove);
+    };
+
+    const touchMove = (ev: TouchEvent) => {
+        initialPos = {
+            x: ev.touches[0].clientX,
+            y: ev.touches[0].clientY,
+        };
+        window.removeEventListener('mousemove', mouseMove);
+        window.removeEventListener('touchmove', touchMove);
+    };
+
+    const mouseUp = (ev: MouseEvent) => {
+        ended(ev.clientX, ev.clientY);
+    };
+
+    const touchEnd = (ev: TouchEvent) => {
+        ended(ev.changedTouches[0].clientX, ev.changedTouches[0].clientY);
+    };
+
+    const ended = (clientX: number, clientY: number) => {
+        destroy();
+
+        if (!initialPos) {
+            if (onCancel) onCancel();
+            return;
+        }
+
+        const deltaX = clientX - initialPos.x;
+        const deltaY = clientY - initialPos.y;
+
+        if (deltaX * deltaX + deltaY*deltaY < deadZone * deadZone) return;
+
+        // horizontal
+        if (Math.abs(deltaX) > Math.abs(deltaY)) {
+            if (deltaX < 0) onSwipe('left');
+            else onSwipe('right');
+        }
+        // vertical
+        else {
+            if (deltaY < 0) onSwipe('up');
+            else onSwipe('down');
+        }
+    };
+
+    window.addEventListener('mousemove', mouseMove);
+    window.addEventListener('mouseup', mouseUp);
+
+    window.addEventListener('touchmove', touchMove);
+    window.addEventListener('touchend', touchEnd);
+
+    const destroy = () => {
+        window.removeEventListener('mousemove', mouseMove);
+        window.removeEventListener('mouseup', mouseUp);
+
+        window.removeEventListener('touchmove', touchMove);
+        window.removeEventListener('touchend', touchEnd);
+    };
+
+    return destroy;
+}
+
+export function listenSwipe(
+    el: HTMLElement|Window,
+    onSwipe: (direction: 'up'|'down'|'left'|'right') => void,
+    deadZone = 15,
+): () => void {
+    let lastActionDestroy: Function;
+
+    const mouseDown = () => {
+        lastActionDestroy = startDetectingScreenSwipe(onSwipe, undefined, deadZone);
+    };
+
+    const touchStart = () => {
+        lastActionDestroy = startDetectingScreenSwipe(onSwipe, undefined, deadZone);
+    };
+
+    el.addEventListener('mousedown', mouseDown);
+    el.addEventListener('touchstart', touchStart);
+
+    const destroy = () => {
+        if (lastActionDestroy) lastActionDestroy();
+        el.removeEventListener('mousedown', mouseDown);
+        el.removeEventListener('touchstart', touchStart);
+    };
+
+    return destroy;
+}
+
+// ----------------------------
+//          END SWIPE
+// ----------------------------
--- src/core/utils.ts
+++ src/core/utils.ts
@@ -1,6 +1,7 @@
 export const $ = <T = HTMLDivElement>(x: string) => document.querySelector(x) as any as T;
 export const $all = <T extends Element = HTMLDivElement>(x: string) => document.querySelectorAll<T>(x);
-const DetectMobileBrowser = require("detect-mobile-browser")(false);
+
+export * from './utils-mobile';
 
 export function intersectionRect(rectA: ClientRect, rectB: ClientRect): { x: number, y: number, width: number, height: number }|false {
     const x = Math.max(rectA.left, rectB.left);
@@ -154,126 +155,6 @@ export function getScroll() {
     return { left, top };
 }
 
-// ----------------------------
-//           SWIPE
-// ----------------------------
-
-export function startDetectingScreenSwipe(
-    onSwipe: (direction: 'up'|'down'|'left'|'right') => void,
-    onCancel?: () => void,
-    deadZone = 15,
-) {
-    let initialPos: undefined|{ x: number, y: number };
-
-    const mouseMove = (ev: MouseEvent) => {
-        initialPos = {
-            x: ev.clientX,
-            y: ev.clientY,
-        };
-        window.removeEventListener('mousemove', mouseMove);
-        window.removeEventListener('touchmove', touchMove);
-    };
-
-    const touchMove = (ev: TouchEvent) => {
-        initialPos = {
-            x: ev.touches[0].clientX,
-            y: ev.touches[0].clientY,
-        };
-        window.removeEventListener('mousemove', mouseMove);
-        window.removeEventListener('touchmove', touchMove);
-    };
-
-    const mouseUp = (ev: MouseEvent) => {
-        ended(ev.clientX, ev.clientY);
-    };
-
-    const touchEnd = (ev: TouchEvent) => {
-        ended(ev.changedTouches[0].clientX, ev.changedTouches[0].clientY);
-    };
-
-    const ended = (clientX: number, clientY: number) => {
-        destroy();
-
-        if (!initialPos) {
-            if (onCancel) onCancel();
-            return;
-        }
-
-        const deltaX = clientX - initialPos.x;
-        const deltaY = clientY - initialPos.y;
-
-        if (deltaX * deltaX + deltaY*deltaY < deadZone * deadZone) return;
-
-        // horizontal
-        if (Math.abs(deltaX) > Math.abs(deltaY)) {
-            if (deltaX < 0) onSwipe('left');
-            else onSwipe('right');
-        }
-        // vertical
-        else {
-            if (deltaY < 0) onSwipe('up');
-            else onSwipe('down');
-        }
-    };
-
-    window.addEventListener('mousemove', mouseMove);
-    window.addEventListener('mouseup', mouseUp);
-
-    window.addEventListener('touchmove', touchMove);
-    window.addEventListener('touchend', touchEnd);
-
-    const destroy = () => {
-        window.removeEventListener('mousemove', mouseMove);
-        window.removeEventListener('mouseup', mouseUp);
-
-        window.removeEventListener('touchmove', touchMove);
-        window.removeEventListener('touchend', touchEnd);
-    };
-
-    return destroy;
-}
-
-export function listenSwipe(
-    el: HTMLElement|Window,
-    onSwipe: (direction: 'up'|'down'|'left'|'right') => void,
-    deadZone = 15,
-): () => void {
-    let lastActionDestroy: Function;
-
-    const mouseDown = () => {
-        lastActionDestroy = startDetectingScreenSwipe(onSwipe, undefined, deadZone);
-    };
-
-    const touchStart = () => {
-        lastActionDestroy = startDetectingScreenSwipe(onSwipe, undefined, deadZone);
-    };
-
-    el.addEventListener('mousedown', mouseDown);
-    el.addEventListener('touchstart', touchStart);
-
-    const destroy = () => {
-        if (lastActionDestroy) lastActionDestroy();
-        el.removeEventListener('mousedown', mouseDown);
-        el.removeEventListener('touchstart', touchStart);
-    };
-
-    return destroy;
-}
-
-// ----------------------------
-//          END SWIPE
-// ----------------------------
-
-let __isMobileHookScreen: boolean|undefined = undefined;
-
-export function isMobileScreen() {
-    return (__isMobileHookScreen !== undefined && __isMobileHookScreen) || DetectMobileBrowser.isAny() || window.innerWidth <= 571;
-}
-
-export function __unsafe_setIsMobileScreen(hook: boolean) {
-    __isMobileHookScreen = hook;
-}
-
 export function sendForm(url: string, form: HTMLFormElement, after: (isOk: boolean) => void) {
     formSubmitXHR(url, JSON.stringify(getFormData(form)), xhr => {
         if (xhr.status === 200) after(true);
@@ -300,33 +181,4 @@ export function getFormData<T = { [field: string]: string }>(form: HTMLFormEleme
     const fieldNodes = form.querySelectorAll('input, textarea');
     const fields: HTMLInputElement[] = Array.prototype.slice.call(fieldNodes);
     return fields.reduce((sum, field) => field.name ? Object.assign(sum, { [field.name]: field.value }) : sum, {} as T);
-}
-
-export function detectTouch(
-    touchDetected: () => void,
-): () => void {
-    const ontouch = () => {
-        destroy();
-        touchDetected();
-    };
-
-    const onpointer = (evt: PointerEvent) => {
-        if (evt.pointerType !== 'mouse') {
-            ontouch();
-        }
-    };
-
-    window.addEventListener('touchstart', ontouch);
-    window.addEventListener('touchmove', ontouch);
-    window.addEventListener('pointerdown', onpointer);
-    window.addEventListener('pointermove', onpointer);
-
-    const destroy = () => {
-        window.removeEventListener('touchstart', ontouch);
-        window.removeEventListener('touchmove', ontouch);
-        window.removeEventListener('pointerdown', onpointer);
-        window.removeEventListener('pointermove', onpointer);
-    };
-
-    return destroy;
 }
