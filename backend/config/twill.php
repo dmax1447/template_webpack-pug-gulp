549c9fea4e8bd62903e6132e2114f13327dc7985
is <igor.sazhnev@gmail.com>
2019-05-07 13:54:10 +0300

    Features блоки для главной + базовая выгрузка страниц

--- backend/config/twill.php
+++ backend/config/twill.php
@@ -17,6 +17,19 @@ return [
                 'icon' => 'image',
                 'component' => 'a17-block-image',
             ],
+            'features_offered' => [
+                'title' => 'Блоки Предлагаем',
+                'icon' => 'text',
+                'component' => 'a17-block-features_offered',
+            ],
+        ],
+        'repeaters' => [
+            'features_offered_item' => [
+                'title' => 'Пункты',
+                'trigger' => 'Добавить пункт',
+                'component' => 'a17-block-features_offered_item',
+                'max' => 10,
+            ],
         ],
         'crops' => [
             'icons' => [
---
+++ backend/resources/views/admin/blocks/features_offered_item.blade.php
@@ -0,0 +1,12 @@
+@formField('input', [
+'name' => 'title',
+'label' => 'Текст',
+'translated' => true,
+'maxlength' => 300
+])
+
+@formField('input', [
+'name' => 'icon',
+'label' => 'Иконка FA',
+'note' => 'FontAwesome icon'
+])
