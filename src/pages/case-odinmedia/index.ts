6ff240ab0c9406d6a3959c6c0d2a29a37ef5e024
morglod <morglod@gmail.com>
2019-05-17 03:28:58 +0300

    cases refactored; odinmedia results fixed; mobile menu fixed; device-gallery updated & reworked

--- src/pages/case-odinmedia/index.ts
+++ src/pages/case-odinmedia/index.ts
@@ -5,14 +5,3 @@ if (process.env.BUILD === 'prod') {
 }
 
 import '../../parts/devices-gallery/code';
-import { $q } from '../../core/utils';
-
-window.addEventListener('load', () => {
-    const rp = $q('.result__previews');
-    rp.addEventListener('mouseenter', () => {
-        rp.classList.add('result__previews--hovered');
-    });
-    rp.addEventListener('mouseleave', () => {
-        rp.classList.remove('result__previews--hovered');
-    });
-});
