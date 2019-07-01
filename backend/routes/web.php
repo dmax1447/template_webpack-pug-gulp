e96176ede0ccba188dfcb935aa45d45dd24685bb
is <igor.sazhnev@gmail.com>
2019-07-01 17:26:38 +0300

    more presentation aliases + en pdf download

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -13,10 +13,37 @@
 
 Route::post('/contact', 'FeedbackController@index');
 
+Route::get('/bereza-{name}-min-{locale}.pdf', function ($name, $locale) {
+    return downloadPresentation($name . '-min', $locale);
+})->where('locale', 'en');
+
+Route::get('/{name}-min-{locale}.pdf', function ($name, $locale) {
+    return downloadPresentation($name . '-min', $locale);
+})->where('locale', 'en');
+
+
+Route::get('/bereza-{name}-{locale}.pdf', function ($name, $locale) {
+    return downloadPresentation($name, $locale);
+})->where('locale', 'en');
+
+Route::get('/bereza-{name}.pdf', function ($name) {
+    return downloadPresentation($name, 'ru');
+});
+
+Route::get('/{name}-{locale}.pdf', function ($name, $locale) {
+    return downloadPresentation($name, $locale);
+})->where('locale', 'en');
+
 Route::get('/{name}.pdf', function ($name) {
+    return downloadPresentation($name, 'ru');
+});
+
+function downloadPresentation($name, $locale = 'ru') {
+    \App::setLocale($locale);
     $map = [
         'pres' => 'presentation', 'pres-min' => 'presentation_min',
-        'presentation' => 'presentation', 'presentation-min' => 'presentation_min'
+        'presentation' => 'presentation', 'presentation-min' => 'presentation_min',
+        'bereza-presentation' => 'presentation', 'bereza-presentation-min' => 'presentation_min',
     ];
     if (!isset($map[$name])) {
         abort(404);
@@ -27,6 +54,5 @@ Route::get('/{name}.pdf', function ($name) {
         ->with('files')->first();
     if (!$el) abort(404);
 
-
-    return response()->file(public_path($el->file($map[$name])));
-});
+    return response()->file(public_path($el->file($map[$name], $locale)));
+}
